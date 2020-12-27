72850875d3f2ee702809af2a4d1716a33e97259a
sameghosts <seanfeiner.brown@gmail.com>
2020-12-27 10:37:05 -0800

    initial files and psuedo code for project

---
+++ main.js
@@ -0,0 +1,25 @@
+// Pinball project breakdown and psuedo code
+
+//Establish Game Canvas object
+/* Game objects
+    Pinball
+    Flippers
+    Initial / Starting Lane
+    Firing Pin
+    Dead space 
+ */
+
+// Map firing pin and flippers to key presses 
+      // determine if timeout's are needed 
+
+// Establish collision detection 
+  // pinball and flippers
+  // pinball and firing pin
+  // pinball and the game board
+  // pin ball and the dead space
+
+// Establish Collision movement / physics
+  // pinball and collision walls 
+  // pinball and collision with bumpers 
+  // initial firing
+  // motion / responsivitiy of the flippers
