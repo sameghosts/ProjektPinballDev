327774c3a893f6ef0d565c5e9a70f026e50b0aaf
sameghosts <seanfeiner.brown@gmail.com>
2020-12-27 10:41:32 -0800

    js psuedo to MVP and stretch

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Pinball project breakdown and psuedo code
 
 //Establish Game Canvas object
-/* Game objects
+/* Game objects:
     Pinball
     Flippers
     Initial / Starting Lane
@@ -23,3 +23,23 @@
   // pinball and collision with bumpers 
   // initial firing
   // motion / responsivitiy of the flippers
+
+// have functioning pinball with flippers firing pin and some kind of object / interaction on board as Minimum Viable Product by Tuesday December 29 9pm PST
+
+//STRETCH GOALS AND FURTHER IMPLEMENTATION
+
+// add lanes with words
+
+// Create a scoring system and bonus conditions
+
+// add circular bumpers
+
+// add targets
+
+// add multiball 
+
+// add music 
+
+// add visual elements
+
+// second level
