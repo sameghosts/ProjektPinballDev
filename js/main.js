a1504eb190dc1cb6b857b21858d590c14f87ffe2
sameghosts <seanfeiner.brown@gmail.com>
2021-01-05 16:18:09 -0800

    collision resolution alg written for walls and pinballs prior to testing taking a break

--- js/main.js
+++ js/main.js
@@ -327,7 +327,7 @@ let flipperB = new flipper(flipBX, flipBY, flipWidth, flipHeight, -flipInitAng, 
   update = (secondsPassed) =>{
     //gravity!!!!
     if (turnOnGrav){
-      console.log(this.pos);
+      // console.log(this.pos);
       this.vy += 9.81 * secondsPassed;
     pinballPostGrav = true;
     }
@@ -469,16 +469,40 @@ function pen_res_PbW (b1, w1){
   b1.pos = b1.pos.add(penVect.unit().mult(b1.r-penVect.mag()));
 
 }
-//collision resolution between pinball and walls
+//collision resolution algorithm between pinball and walls with restitution slight decrease in energy
+const restitution = 0.9;
 function coll_res_PbW (b1, w1){
+  let closestPoint = closestPointPbW(b1,w1);
+  console.log(closestPoint);
+  let ballToClosest = closestPointPbW(b1, w1).subtr(b1.pos);
+  if(b1.pos.x > closestPoint.x){
+    b1.vx = Math.abs(b1.vx) * restitution;
+    b1.pos.x = closestPoint.x + b1.r;
+  } else if (b1.pos.x < closestPoint.x){
+    b1.vx= -Math.abs(b1.vx) * restitution;
+    b1.pos.x = closestPoint.x -b1.r;
+  }
+
+  if(b1.pos.y < closestPoint.y){
+    b1.vy = Math.abs(b1.vy) * restitution;
+    b1.pos.y = closestPoint.y + b1.r;
+  } else if (b1.pos.y > closestPoint.y){
+    b1.vy = -Math.abs(b1.vy) * restitution;
+    b1.pos.y = closestPoint.y + b1.r
+  }
+  }
+
+  console.log(ballToClosest);
   //come back to this mathematics after you have refactored pinball physics and update ##### 
-  let normal = b1.pos.subtr(closestPointPbW(b1,w1)).unit();
-  let sepVel = Vector.dot(b1.vel, normal);
-  let new_sepVel = -sepVel * b1.elasticity;
-  let vsep_diff = sepVel - new_sepVel;
-  b1.vel = b1.vel.add(normal.mult(-vsep_diff));
+  // let normal = b1.pos.subtr(closestPointPbW(b1,w1)).unit();
+  // let sepVel = Vector.dot(b1.vel, normal);
+  // let new_sepVel = -sepVel * b1.elasticity;
+  // let vsep_diff = sepVel - new_sepVel;
+  // b1.vel = b1.vel.add(normal.mult(-vsep_diff));
   // console.log("add pb vel " + normal.mult(-vsep_diff));
   // b1.reposition;
+
+
 }
 //collision detection pinball and flippers
 //pen resolution between pinball and flippers
@@ -517,6 +541,11 @@ function coll_res_PbW (b1, w1){
     turnOnGrav = true;
     let wallLaneGate = new Wall(500, 175, game.clientWidth, 100);
   }
+  if(pinball1.pos.y - pinball1.r <= 0){
+    pinball1.pos.y=pinball1.r;
+    pinball1.vy = 1;
+    pinball1.vx = -10;
+  }
 
   //calc frames per second
   fps = Math.round(1 / secondsPassed);
@@ -554,14 +583,16 @@ function coll_res_PbW (b1, w1){
       console.log(pinball1.pos);
     }
     if (coll_det_PbW){
-      console.log(pinballHeightPass)
+      // console.log(pinballHeightPass)
       // console.log(pinball.pos.y+pinball.r);
     }
     //wall iteration draw and coll detection and response
     wallsArr.forEach((w) =>{
       if(coll_det_PbW(pinball1, w)){
+        ctx.fillText("Collision", 300, 300);
+
         // pen_res_PbW(pinball1, w);
-        // coll_res_PbW(pinball1, w);
+        coll_res_PbW(pinball1, w);
         // pinball1.reposition();
       }
       })
